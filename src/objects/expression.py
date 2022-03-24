a3c8cfe59574703fe7a6468f09523532ce6aea68
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2022-03-24 20:56:28 +0800

    Add Binrary Expressions

--- src/objects/expression.py
+++ src/objects/expression.py
@@ -3,3 +3,9 @@ class Expression:
         self.path = path
         self.op = op 
         self.value = value
+
+class BinaryExpression:
+    def __init__(self, expr_1, binary_op, expr_2):
+        self.expr_1 = expr_1
+        self.binary_op = binary_op
+        self.expr_2 = expr_2
