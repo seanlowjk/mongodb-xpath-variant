e9ee7500f65f3260917201a3866f0da3e3471845
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2022-03-25 15:00:52 +0800

    Add Tests

--- src/tests_parser.py
+++ src/tests_parser.py
@@ -1,4 +1,5 @@
 from utils.parser import Parser
 
-p = Parser('/child::a[child::b < "j k" or child::c > 3 and child::d = 0]/child::e[child::f != 0]')
+p = Parser('/child::x/child::a[child::d = 0]/child::e[child::f != 0]')
 p.run() 
+
