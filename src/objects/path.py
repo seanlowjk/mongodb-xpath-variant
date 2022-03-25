27c50c60591d294fe58d8ed0df7b62edef8733d7
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2022-03-25 15:26:42 +0800

    Fix Path

--- src/objects/path.py
+++ src/objects/path.py
@@ -5,6 +5,7 @@ class Path:
     def __init__(self, str_path):
         self.str_path =str_path
         self.levels = str_path.split(STEP_STARTER)
+        self.levels.pop(0)
 
     def __str__(self):
         return self.str_path
