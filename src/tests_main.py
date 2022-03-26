1ed5aee59f0623067b1fd276cb7b278d2fdb9551
GOH Si Ning <51023107+gohsnn@users.noreply.github.com>
2022-03-27 01:43:37 +0800

    Add incomplete evaluation for CHILD axis

--- src/tests_main.py
+++ src/tests_main.py
@@ -3,5 +3,6 @@ from utils.executor import Executor
 
 inferencer = Executor("json/basic.json")
 data = inferencer.get_json_data()
+schema = inferencer.get_schema()
 
-print(data)
+# print(data)
--- src/utils/executor.py
+++ src/utils/executor.py
@@ -1,40 +1,65 @@
 from json import load
-from pydantic import Json
 from pyjsonq import JsonQ
+from genson import SchemaBuilder
+
+import constants
 
 class Executor:
     def __init__(self, json_file_path):
         self.json_file_path = json_file_path
         self.qe = JsonQ(self.json_file_path)
+        self.builder = SchemaBuilder()
 
     def get_json_data(self):
-        json_data = load(open(self.json_file_path))
-        print(json_data.keys())
-    
+        self.json_data = load(open(self.json_file_path))
 
-    # def evaluate_data(self, parsed_info):
-    #     levels, exprs = parsed_info # ['child::x', 'child::a', 'child::e'] - paths.get_levels(), [(2:expr), (3:expr)]
-    #     i = 1 
-    #     for level in levels:
-    #         if i in exprs:
-    #             res = self.get_cur_res(level, exprs.get(i))
-    #         else:
-    #             res = self.get_cur_res(level)
-    #         i += 1
+    '''
+    Retrieves schema in JSON format in self.keys
+    '''
+    def get_schema(self):
+        self.builder.add_object(self.json_data)        
+        schema = self.builder.to_schema()
+        qe = JsonQ(data=schema)
+        self.keys = qe.at('properties')
+        # print(self.keys.get())
 
-    #     res = self.qe.at('x').at('a').where('d', '=', "3").get()
-    #     return res
+        # next_level = self.keys.at('x').at('properties').at('a')
+        # next level down should either be items or properties (check their keys first)
+        # print(next_level.get().keys())
+        # print(self.keys.get())
 
-    # def get_cur_res(level, expr=None):
-    #     # attr = get_attribute(level)
-    #     # expr:
-    #     # - remove child::
-    #     # - split into 3 part (key, operator, value)
-    #     if expr is None:
-    #         res = self.qe.at(level).get()
-    #     else:
-    #         res = self.qe.at(level).where(expr).get()
-    #     return res
+    def evaluate_data(self, parsed_info):
+        levels, exprs = parsed_info
+        expr_iter = 0
+        self.curr_path = ''
+        for level in levels:
+            if expr_iter >= len(exprs):
+                self.curr_path = self.get_cur_res(level)
+            else: 
+                new_expr = exprs.at(expr_iter)
+                # check what kind of expr it is (unary or binary), need different API
+                path, op, value, value_type = new_expr.to_parts()
+                if path == self.curr_path:
+                    self.curr_path = self.get_cur_res(level, new_expr)
+                    expr_iter += 1
+                else: 
+                    self.curr_path = self.get_cur_res(level)
+
+        return self.curr_path.get()
+
+    def get_cur_res(level, expr=None):
+        level_arr = level.split('::')
+        is_child = False
+        if level_arr[0] == constants.Axes.CHILD:
+            is_child = True
+
+        if is_child:
+            if expr is None:
+                self.curr_path = self.qe.at(level)
+            else:
+                # check type of expr
+                path, op, value, value_type = expr.to_parts()
+                self.curr_path = self.qe.at(level).where(op, value_type, value) # confirm what the prev method returns
 
     def has_path(self, path):
         return True 
