09e90386b3cd1f6c1e296a1e0322e86a643e4ad4
GOH Si Ning <51023107+gohsnn@users.noreply.github.com>
2022-03-26 21:10:21 +0800

    Add pyjsonq package

--- src/tests_main.py
+++ src/tests_main.py
@@ -1,15 +1,7 @@
 from json import load, dumps
 from utils.executor import Executor
 
-inferencer = Executor(open("json/basic.json"))
-schema = inferencer.get_schema()
-data = inferencer.get_data()
+inferencer = Executor("json/basic.json")
+data = inferencer.get_json_data()
 
-print(dumps(data, indent=2))
-print(type(data["x"]) == dict)
-print(type(data["x"]["a"]))
-
-# print(dumps(schema, indent=2))
-# print(dumps(schema["properties"], indent=2))
-# a = a["x"]["a"]["b"]
-# print(dumps(a, indent=2))
+print(data)
--- src/utils/executor.py
+++ src/utils/executor.py
@@ -1,11 +1,40 @@
 from json import load
+from pydantic import Json
+from pyjsonq import JsonQ
 
 class Executor:
-    def __init__(self, json_file_data):
-        self.json_data = load(json_file_data)
+    def __init__(self, json_file_path):
+        self.json_file_path = json_file_path
+        self.qe = JsonQ(self.json_file_path)
+
+    def get_json_data(self):
+        json_data = load(open(self.json_file_path))
+        print(json_data.keys())
     
-    def get_data(self):
-        return self.json_data
+
+    # def evaluate_data(self, parsed_info):
+    #     levels, exprs = parsed_info # ['child::x', 'child::a', 'child::e'] - paths.get_levels(), [(2:expr), (3:expr)]
+    #     i = 1 
+    #     for level in levels:
+    #         if i in exprs:
+    #             res = self.get_cur_res(level, exprs.get(i))
+    #         else:
+    #             res = self.get_cur_res(level)
+    #         i += 1
+
+    #     res = self.qe.at('x').at('a').where('d', '=', "3").get()
+    #     return res
+
+    # def get_cur_res(level, expr=None):
+    #     # attr = get_attribute(level)
+    #     # expr:
+    #     # - remove child::
+    #     # - split into 3 part (key, operator, value)
+    #     if expr is None:
+    #         res = self.qe.at(level).get()
+    #     else:
+    #         res = self.qe.at(level).where(expr).get()
+    #     return res
 
     def has_path(self, path):
         return True 
